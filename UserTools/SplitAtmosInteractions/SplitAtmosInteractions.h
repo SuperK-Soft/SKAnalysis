0aa004d77918c90ee49371761804e63803f33ad3
Matthew Nicholson <m.nicholson1211@gmail.com>
2025-02-08 01:28:56 +0900

    added plotting to split tool, plus output file

--- UserTools/SplitAtmosInteractions/SplitAtmosInteractions.h
+++ UserTools/SplitAtmosInteractions/SplitAtmosInteractions.h
@@ -25,15 +25,35 @@ class SplitAtmosInteractions: public Tool {
 
  private:
 
-  MTreeReader* tree_reader_ptr = nullptr;
-  void GetReader();
+  MTreeReader* tree_reader_root_ptr = nullptr;
+  MTreeReader* tree_reader_ZBS_ptr = nullptr;
 
-  TH1D interaction_mode_plot;
-  TH1D interaction_mode_zero;
-  TH1D NCQE_plot;
-  TH1D non_NCQE_plot;
+  TFile* output_file_ptr =  nullptr;
+  TTree* output_tree_ptr = nullptr;
 
-  std::map<std::string, int> interactions_for_pi_chart;
+  LoweInfo* lowe_ptr = nullptr;
+  MCInfo* mcinfo_ptr = nullptr;
+  Header* header_ptr = nullptr;
+  TQReal* tqreal_ptr = nullptr;
+  TQReal* tqareal_ptr = nullptr;
+  SecondaryInfo* secondary_info_ptr = nullptr;
+
+  std::vector<float>*  neutron_likelihoods_ptr;
+  int interaction_mode = 0;
+  
+  void GetReaders();
+  void GetValues();
+  void UpdateBranches();
+  void SetOutputBranches(TTree*);
+  
+  bool first = true;
+  
+  // TH1D interaction_mode_plot;
+  // TH1D interaction_mode_zero;
+  // TH1D NCQE_plot;
+  // TH1D non_NCQE_plot;
+
+  // std::map<std::string, int> interactions_for_pi_chart;
 
 };
 
